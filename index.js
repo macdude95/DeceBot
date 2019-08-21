b6717a4dbe64eda99dfe9cc762f2cb8017117798
Michael Cantrell <michaelcantrell@Michaels-MacBook-Pro.local>
2019-08-20 18:24:50 -0700

    permissions on sound effects and timeout on say

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var tmi = require('tmi.js');
 var say = require('say');
 var player = require('play-sound')(opts = {});
+var rf = require('random-facts'); // Require the package
 
 var options = {
 	options: {
@@ -21,46 +22,84 @@ var client = new tmi.client(options);
 client.connect();
 
 client.on("connected", function(a,p) {
-	client.action("itsdece", "SUP");
+	client.action("itsdece", "SUP WORLD!");
 });
 
 var sound_effects = {
-	"Tuturu": "Tuturu.mp3", 
-	"Oof": "Roblox Death Sound Effect.mp3",
-	"MonkaGigaDude": "monka giga dude.mp3",
-	"MissedIt": "missed it by that much.mp3",
-	"Dust": "another one bites the dust.mp3"
+	"Tuturu": {
+		path: "Tuturu.mp3",
+		userPermissionsList: []
+	}, 
+	"Oof": {
+		path: "Roblox Death Sound Effect.mp3",
+		userPermissionsList: []
+	}, 
+	"MonkaGigaDude": {
+		path: "monka giga dude.mp3",
+		userPermissionsList: ["itsdece", "br4c3_dk"]
+	}, 
+	"MissedIt": {
+		path: "missed it by that much.mp3",
+		userPermissionsList: []
+	}, 
+	"Dust": {
+		path: "another one bites the dust.mp3",
+		userPermissionsList: []
+	}, 
 };
 
+var sayTimeout = 60000;
+var sayTimeoutList = {};
+
 client.on("chat", function(channel, userstate, message, self) {
-	console.log(channel);
 	if(message.includes("!say")) {
 		speak_text(userstate.username, message.replace("!say", ""));
-	}
-	else if (sound_effect_command_in_message(message) != "") {
-		play_sound(sound_effect_command_in_message(message));
+	} else if (message.includes("!randomfact")) {
+		client.action("itsdece", rf.randomFact());
+	} else if (sound_effect_command_in_message(message) != "") {
+		play_sound(sound_effect_command_in_message(message), userstate.username);
 	} else {
-		console.log("Nothing to do here...");
+		//console.log("Nothing to do here...");
 	}
 });
 
 function sound_effect_command_in_message(message) {
 	for (const key of Object.keys(sound_effects)) {
-	  if (message.toLowerCase().includes("!" + key.toLowerCase())) {
-		return sound_effects[key];
+		if (message.toLowerCase().includes("!" + key.toLowerCase())) {
+			return sound_effects[key];
 		}
 	}
 	return "";
 }
 
-function speak_text(username, message) { 
+function speak_text(username, message) {
+	if (sayTimeoutList[username]) {
+		var lastTime = sayTimeoutList[username];
+		var elapsedTime = Date.now() - lastTime;
+		if (elapsedTime < sayTimeout) {
+			client.action("itsdece", `Yo ${username}, chill out with the !say command for at least another ${Math.ceil((sayTimeout - elapsedTime)/1000)} seconds`);
+			return;
+		}
+	}
+	sayTimeoutList[username] = Date.now();
 	say.speak(username + " says: " + message, "Microsoft Zira Desktop");
 }
 
-function play_sound(sound_effect_path) {
+function user_is_allowed_to_use_sound_effect(sound_effect, username) {
+	if (!sound_effect.userPermissionsList.length) {
+		return true;
+	} 
+	return sound_effect.userPermissionsList.includes(username);
+}
+
+function play_sound(sound_effect, username) {
+	if (!user_is_allowed_to_use_sound_effect(sound_effect, username)) {
+		return;
+	}
+
 	var pathToSoundEffects = "C:\\Users\\Michael\\Desktop\\StreamStuff\\SoundEffects\\";
 
-	player.play(pathToSoundEffects + sound_effect_path, function(err){
+	player.play(pathToSoundEffects + sound_effect.path, function(err){
 	  if (err) throw err
 	});
 }
