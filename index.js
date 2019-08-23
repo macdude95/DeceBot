76860e1bdadca40f2d750cbfa6b426848cf5aef5
Michael Cantrell <michcant@amazon.com>
2019-08-23 09:18:45 -0700

    time limit on speech

--- index.js
+++ index.js
@@ -6,94 +6,102 @@ const SoundEffect = require('./SoundEffect.js')
 const TwitchClient = require('twitch').default; //https://www.npmjs.com/package/twitch
 
 var options = {
-	options: {
-		debug: false
-	},
-	connection: {
-		cluster: "aws",
-		reconnect: true
-	},
-	identity: {
-		username: "DeceBot",
-		password: "oauth:7fgcs39add7sai7jibewmxzr6b7ydq"
-	},
-	channels: ["itsdece"]
+    options: {
+        debug: false
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        username: "DeceBot",
+        password: "oauth:7fgcs39add7sai7jibewmxzr6b7ydq"
+    },
+    channels: ["itsdece"]
 }
 
 var client = new tmi.client(options);
 client.connect();
 
 client.on("connected", function(a,p) {
-	client.action("itsdece", "SUP WORLD!");
+    client.action("itsdece", "SUP WORLD!");
 });
 
 var soundEffects = {
-	"!Tuturu": new SoundEffect("!Tuturu", "Tuturu.mp3"),
-	"!Oof": new SoundEffect("!Oof", "Roblox Death Sound Effect.mp3"),
-	"!MonkaGigaDude": new SoundEffect("!MonkaGigaDude", "monka giga dude.mp3", ["itsdece", "br4c3_dk"]),
-	"!MissedIt": new SoundEffect("!MissedIt", "missed it by that much.mp3"),
-	"!Dust": new SoundEffect("!Dust", "another one bites the dust.mp3")
+    "!Tuturu": new SoundEffect("!Tuturu", "Tuturu.mp3"),
+    "!Oof": new SoundEffect("!Oof", "Roblox Death Sound Effect.mp3"),
+    "!MonkaGigaDude": new SoundEffect("!MonkaGigaDude", "monka giga dude.mp3", ["itsdece", "br4c3_dk"]),
+    "!MissedIt": new SoundEffect("!MissedIt", "missed it by that much.mp3"),
+    "!Dust": new SoundEffect("!Dust", "another one bites the dust.mp3")
 };
 
 var sayTimeout = 60000;
 var sayTimeoutList = {};
 
 client.on("chat", function(channel, userstate, message, self) {
-	console.log(userstate);
-	TwitchClient.getUser(userstate["user-id"]);
-	if(message.includes("!say")) {
-		speakText(userstate.username, message.replace("!say", ""));
-	} else if (message.includes("!randomfact")) {
-		client.action("itsdece", rf.randomFact());
-	} else if (soundEffectCommandInMessage(message)) {
-		playSound(soundEffectCommandInMessage(message), userstate.username);
-	} else {
-		//console.log("Nothing to do here...");
-	}
+    // console.log(userstate);
+    // TwitchClient.getUser(userstate["user-id"]);
+    if(message.includes("!say")) {
+        speakText(userstate.username, message.replace("!say", ""));
+    } else if (message.includes("!randomfact")) {
+        client.action("itsdece", rf.randomFact());
+    } else if (soundEffectCommandInMessage(message)) {
+        playSound(soundEffectCommandInMessage(message), userstate.username);
+    } else {
+        //console.log("Nothing to do here...");
+    }
 });
 
 function soundEffectCommandInMessage(message) {
-	for (const key of Object.keys(soundEffects)) {
-		if (message.toLowerCase().includes(key.toLowerCase())) {
-			return soundEffects[key];
-		}
-	}
-	return null;
+    for (const key of Object.keys(soundEffects)) {
+        if (message.toLowerCase().includes(key.toLowerCase())) {
+            return soundEffects[key];
+        }
+    }
+    return null;
 }
 
 function speakText(username, message) {
-	// TODO: Limit spoken text to certain character length, and only allow natural langauge of at least some minimum score.
-	if (message.length)
-	if (sayTimeoutList[username]) {
-		var lastTime = sayTimeoutList[username];
-		var elapsedTime = Date.now() - lastTime;
-		if (elapsedTime < sayTimeout) {
-			client.action("itsdece", `Yo ${username}, chill out with the !say command for at least another ${Math.ceil((sayTimeout - elapsedTime)/1000)} seconds`);
-			return;
-		}
-	}
-	sayTimeoutList[username] = Date.now();
-	say.speak(username + " says: " + message, "Microsoft Zira Desktop", 1.0, function(err){
-		if (err) {
-			console.log(`FAILED attempt to speak: ${message}`)
-		} else {
-			console.log(`SUCCESS text to speech: ${message}`)
-		}
-	});
+    // TODO: Limit spoken text to certain character length, and only allow natural langauge of at least some minimum score.
+    if (message.length)
+    if (sayTimeoutList[username]) {
+        var lastTime = sayTimeoutList[username];
+        var elapsedTime = Date.now() - lastTime;
+        if (elapsedTime < sayTimeout) {
+            client.action("itsdece", `Yo ${username}, chill out with the !say command for at least another ${Math.ceil((sayTimeout - elapsedTime)/1000)} seconds`);
+            return;
+        }
+    }
+    sayTimeoutList[username] = Date.now();
+    say.speak(message, "Microsoft Zira Desktop", 1.0, function(err){
+        if (err) {
+            console.log(`FAILED attempt to speak: ${message}`)
+        } else {
+            console.log(`SUCCESS text to speech: ${message}`)
+        }
+    });
+
+    setTimeout(() => {
+      say.stop((err) => {
+        if (!err) {
+          console.log('Stopped Speech');
+        }        
+      });
+    }, 2000)
 }
 
 function playSound(soundEffect, username) {
-	if (!soundEffect.isAllowedFor(username)) {
-		return;
-	}
+    if (!soundEffect.isAllowedFor(username)) {
+        return;
+    }
 
-	var pathToSoundEffects = "C:\\Users\\Michael\\Desktop\\StreamStuff\\SoundEffects\\";
+    var pathToSoundEffects = "C:\\Users\\Michael\\Desktop\\StreamStuff\\SoundEffects\\";
 
-	player.play(pathToSoundEffects + soundEffect.filePath, function(err){
-		if (err) {
-			console.log(`FAILED attempt to play: ${soundEffect.filePath}`)
-		} else {
-			console.log(`SUCCESS played audio: ${soundEffect.filePath}`)
-		}
-	});
+    player.play(pathToSoundEffects + soundEffect.filePath, function(err){
+        if (err) {
+            console.log(`FAILED attempt to play: ${soundEffect.filePath}`)
+        } else {
+            console.log(`SUCCESS played audio: ${soundEffect.filePath}`)
+        }
+    });
 }
